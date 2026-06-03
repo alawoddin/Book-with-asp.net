b1f242402524a686e90fe9e69bbc02158c4f8bdc
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-06-03 13:32:56 +0430

    i create the categroy create page

--- Book/Controllers/CategoryController.cs
+++ Book/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Book.Data;
+using Book.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Book.Controllers
@@ -16,5 +17,92 @@ namespace Book.Controllers
             var categories = _context.Categories.ToList();
             return View("Index" , categories);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Create")]
+        public IActionResult CreatePOST(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category Insert successfully";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+           
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Update")]
+        public IActionResult UpdatePOST(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category Update successfully";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+
+            TempData["success"] = "Category deleted successfully";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
--- Book/Models/Category.cs
+++ Book/Models/Category.cs
@@ -1,8 +1,13 @@
-﻿namespace Book.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
         public int DisplayOrder { get; set; }
     }
