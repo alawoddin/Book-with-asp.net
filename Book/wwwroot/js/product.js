a0af0f338c587e98653e4f77df2e7aadc192436f
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-06-06 22:45:16 +0430

    show data in table

--- Book/wwwroot/js/product.js
+++ Book/wwwroot/js/product.js
@@ -1,1 +1,13 @@
-﻿
+﻿$('#myTable').DataTable({
+    ajax: '/product/getall',
+    columns: [
+        { data: "title" },
+        { data: "isbn" },
+        { data: "price" },
+        { data: "author" },
+        { defaultContent: '' },
+
+        { defaultContent: '' },
+       
+    ]
+});
