e59dbe97f5dd13cfd4eb2a30897f2142d2f2f97f
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-06-11 14:57:00 +0430

    i create the index style

--- Book/wwwroot/js/product.js
+++ Book/wwwroot/js/product.js
@@ -1,13 +1,30 @@
 ﻿$('#myTable').DataTable({
     ajax: '/product/getall',
     columns: [
-        { data: "title" },
-        { data: "isbn" },
-        { data: "price" },
-        { data: "author" },
-        { data: 'category.name' },
+        { data: "title" , "width": "25%" },
+        { data: "isbn", "width": "15%" },
+        { data: "price", "width": "10%", "render": function (data) { return '$' + data.toFixed(2); } },
+        { data: "author", "width": "15%" },
+        {
+            data: 'category.name', "width": "10%", "render": function (data)
+            { return '<span class="badge bg-info">' + data + '</span>'; }
+        },
 
-        { defaultContent: '' },
+        {
+            data: 'id', "width": "25%", "render": function (data)
+            {
+                return `<div class="d-flex gap-2 justify-content-end">
+                    <a href="/product/edit?id=${data}" class="btn btn-sm btn-outline-success">
+                    <i class="bi bi-pencil-square"></i>Edit
+                    </a>
+
+                     <a href="/product/edit?id=${data}" class="btn btn-sm btn-outline-danger">
+                    <i class="bi bi-trash"></i>Delete
+                    </a>
+
+                </div>`;
+            }
+        },
        
     ]
 });
